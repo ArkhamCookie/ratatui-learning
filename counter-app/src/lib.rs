9e895aa59aaecbe4975666979fe7e24230945ca3
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-16 00:39:54 -0500

    Feat: Implement base draw/rendering

---
+++ counter-app/src/lib.rs
@@ -0,0 +1,70 @@
+use std::{io, vec};
+
+use ratatui::{
+	buffer::Buffer,
+	layout::{Alignment, Rect},
+	style::Stylize,
+	symbols::border,
+	text::{Line, Text},
+	widgets::{
+		block::{Position, Title},
+		Block, Paragraph, Widget,
+	},
+	DefaultTerminal, Frame,
+};
+
+#[derive(Debug, Default)]
+pub struct App {
+	counter: u8,
+	exit: bool,
+}
+
+impl App {
+	fn draw(&self, frame: &mut Frame) {
+		frame.render_widget(self, frame.area());
+	}
+
+	fn handle_events(&mut self) -> io::Result<()> {
+		// todo!()
+		Ok(())
+	}
+
+	pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+		while !self.exit {
+			terminal.draw(|frame| self.draw(frame))?;
+			self.handle_events()?;
+		}
+		Ok(())
+	}
+}
+
+impl Widget for &App {
+	fn render(self, area: Rect, buf: &mut Buffer) {
+		let title = Title::from(" Counter App Tutorial ".bold());
+		let instructions = Title::from(Line::from(vec![
+			"Decrement ".into(),
+			"<Left>".blue().bold(),
+			" Increment ".into(),
+			"<Right>".blue().bold(),
+			" Quit ".into(),
+			"<Q> ".blue().bold(),
+		]));
+		let block = Block::bordered()
+			.title(title.alignment(Alignment::Center))
+			.title(
+				instructions
+					.alignment(Alignment::Center)
+					.position(Position::Bottom),	
+			)
+			.border_set(border::THICK);
+		let counter_text = Text::from(vec![Line::from(vec![
+			"Value: ".into(),
+			self.counter.to_string().yellow(),
+		])]);
+
+		Paragraph::new(counter_text)
+			.centered()
+			.block(block)
+			.render(area, buf);
+	}
+}
--- counter-app/src/main.rs
+++ counter-app/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-	println!("Hello, world!");
+use std::io;
+
+use counter_app::App;
+
+fn main() -> io::Result<()> {
+	let mut terminal = ratatui::init();
+	let app_result = App::default().run(&mut terminal);
+
+	ratatui::restore();
+	app_result
 }
